af864d24a50ecafe66970129d9f1b1934386150e
Huma-Mohsin <160951214+Huma-Mohsin@users.noreply.github.com>
2024-10-17 02:31:50 +0500

    first commit

---
+++ src/app/usersdata/loading.tsx
@@ -0,0 +1,6 @@
+const LoadingPage=()=>{
+    return (
+        <h1 style={{color:"red",fontWeight:"bolder",fontSize:"20px",padding:"20px",margin:"20px"}}>Please Wait, Page is Loading....</h1>
+    )
+}
+export default LoadingPage
